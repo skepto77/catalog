f1a12deff426e246f513c0a54f0ffb3043d48b46
Ilya Grachev <ilya095@gmail.com>
2021-08-08 21:22:29 +0300

    Create search.tsx

---
+++ pages/search.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { GetStaticProps } from 'next';
+import { withLayout } from '../layout/Layout';
+import axios from 'axios';
+import { MenuItem } from '../interfaces/menu.interface';
+import { Menu } from '../layout/Menu/Menu';
+
+function Search({menu, firstCategory} :SearchProps): JSX.Element {
+  return (
+    <>
+      search page
+    </>
+  );
+}
+
+export default withLayout(Search);
+
+export const getStaticProps: GetStaticProps<SearchProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find', {
+    firstCategory
+  });
+  return {
+    props: {
+      menu,
+      firstCategory,
+    }
+  };
+};
+
+interface SearchProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}
